6b12ca1bd4c6cbea12eaa2f7a1a92f06075a96ee
Barry Low <barry@studiobear.co>
2020-01-18 22:46:44 -0800

    Remove gatsby-theme ionic lib files

--- packages/gatsby-theme-ionic/lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var layouts_1 = require("./layouts");
-exports.Layout = layouts_1.default;
-var header_1 = require("./components/header");
-exports.Header = header_1.default;
-//# sourceMappingURL=index.js.map
--- packages/gatsby-theme-ionic/lib/utils/typography.d.ts
+++
@@ -1,5 +0,0 @@
-import Typography from 'typography';
-declare const typography: Typography;
-export default typography;
-export declare const rhythm: (value: number) => string;
-export declare const scale: (value: number) => import("typography").BaseLine;
