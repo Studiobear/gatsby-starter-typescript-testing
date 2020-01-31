7d11db3b2ded1b8df44c3b41476534b43f2573dc
Barry Low <barry@studiobear.co>
2020-01-31 11:35:24 -0800

    fix(shared-ui-ionic): Layout test refactored for loadable

--- packages/shared-ui-ionic/src/layouts/__tests__/layout.spec.tsx
+++ packages/shared-ui-ionic/src/layouts/__tests__/layout.spec.tsx
@@ -11,34 +11,12 @@ describe('Layout', () => {
 
   afterEach(cleanup)
 
-  it('renders without crashing', () => {
+  it('Shows loading element', () => {
     const { getByText } = render(
       <Layout title={siteTitle}>
         <h1>Renders</h1>
       </Layout>,
     )
-    expect(getByText('Renders')).toBeInTheDocument()
-  })
-
-  it('passes data and children props', () => {
-    const { getByText, queryAllByTestId } = render(
-      <Layout title={siteTitle}>
-        <h1>Passes props</h1>
-      </Layout>,
-    )
-    expect(queryAllByTestId('banner').length).toBe(1)
-    expect(getByText('Gatsby Starter Ionic - Layout')).toBeInTheDocument()
-    expect(getByText('Passes props')).toBeInTheDocument()
-  })
-
-  it('passes defaultHref to header', () => {
-    const { queryAllByTestId } = render(
-      <Layout title={siteTitle} defaultHref="/blog">
-        <h1>Passes defaultHref</h1>
-      </Layout>,
-    )
-    const testNav = queryAllByTestId('header-nav-back')
-    expect(testNav.length).toBe(1)
-    expect(testNav[0]).toMatchObject(expect.objectContaining({ defaultHref: '/blog' }))
+    expect(getByText('Loading...')).toBeInTheDocument()
   })
 })
